78f161266be2b9749f989671fc56ff246d9c1b22
wooseok <nomindz@naver.com>
2013-05-02 12:53:13 +0900

    213

--- src/com/example/testandroid/testUpstream.java
+++ src/com/example/testandroid/testUpstream.java
@@ -3,5 +3,8 @@ package com.example.testandroid;
 public class testUpstream {
 	public void testFunc(){
 		System.out.println("sdlfkjlek");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
 	}
 }
